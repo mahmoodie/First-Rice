41b9ca31d949cffe29fa3b5303fdc80cd42767eb
mahmoodie <104816636+mahmoodie@users.noreply.github.com>
2024-12-07 09:12:12 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+yes | sudo pacman -Syu
+yes | sudo pacman -S thunar firefox bspwm polybar picom rofi kitty fish nitrogen sxhkd fastfetch
+cd /dotfiles
+mv bspwm fastfetch fish kitty kitty-bak neofetch picom polybar rofi sxhkd $HOME/.config
+cd
+cd /.config/bspwm
+chmod +x bspwmrc
+cd
+cd /.config/sxhkd
+chmod +x sxhkdrc
