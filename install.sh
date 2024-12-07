a2a8b86c2e5e6d0c40e0d3ca5dba154e4a47a4b1
mahmoodie <104816636+mahmoodie@users.noreply.github.com>
2024-12-07 09:13:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 yes | sudo pacman -Syu
 yes | sudo pacman -S thunar firefox bspwm polybar picom rofi kitty fish nitrogen sxhkd fastfetch
-cd /dotfiles
+cd First-Rice/dotfiles
 mv bspwm fastfetch fish kitty kitty-bak neofetch picom polybar rofi sxhkd $HOME/.config
 cd
 cd /.config/bspwm
