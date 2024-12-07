29b0b1005ce90f8942ddbd98693730a78e3df16f
mahmoodie <104816636+mahmoodie@users.noreply.github.com>
2024-12-07 08:47:09 +0300

    Update config.fish

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,9 +1,7 @@
-alias py=python
-alias please=sudo
-alias nd=node
-alias pm=pacman
+alias py='python'
+alias please='sudo'
+alias nd='node'
+alias pm='pacman'
 
 # Created by `pipx` on 2024-03-22 22:00:46
 set PATH $PATH /home/riviox/.local/bin
-
-alias fastfetch-custom='fastfetch --kitty-direct /home/Mahmoud/Downloads/neofetch-themes-main/normal/Arch-Fetch/Arch-fetch.png --logo-width 28 --logo-height 18'
